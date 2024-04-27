fed0ebb802601543c2fb2e0adc6ab48c30a49a08
codaaa19 <tsabitiphone@gmail.com>
2024-04-27 19:21:38 +0700

    Applying table without having padding for the last cell

---
+++ src/app/royalty/page.tsx
@@ -0,0 +1,252 @@
+export default function Royalti(){
+    return (
+<div className="flex justify-center items-center h-screen bg-gray-625" style={{ paddingTop: 'var(--header-height, 5rem)' }}>
+  <div className="overflow-hidden shadow-md sm:rounded-lg w-3/4 lg:w-1/2">
+    <table className="min-w-full divide-y divide-gray-200">
+      <thead className="bg-gray-50">
+        <tr>
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Judul Lagu
+          </th>
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Judul Album
+          </th>
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Total Play
+          </th>
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Total Download
+          </th>
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Total Royalti Didapat
+          </th>
+        </tr>
+      </thead>
+      <tbody className="bg-white divide-y divide-gray-200">
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Lagu1
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Album1
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            3
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            0
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Rp 450000
+          </td>
+        </tr>
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Lagu2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Album2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Rp 520000
+          </td>
+        </tr>
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Lagu2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Album2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Rp 520000
+          </td>
+        </tr>
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Lagu2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Album2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Rp 520000
+          </td>
+        </tr>
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Lagu2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Album2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Rp 520000
+          </td>
+        </tr>
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Lagu2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Album2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Rp 520000
+          </td>
+        </tr>
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Lagu2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Album2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Rp 520000
+          </td>
+        </tr>
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Lagu2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Album2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Rp 520000
+          </td>
+        </tr>
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Lagu2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Album2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Rp 520000
+          </td>
+        </tr>
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Lagu2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Album2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Rp 520000
+          </td>
+        </tr>
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Lagu2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Album2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Rp 520000
+          </td>
+        </tr>
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Lagu2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Album2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Rp 520000
+          </td>
+        </tr>
+        <tr>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Lagu2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            Album2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap">
+            2
+          </td>
+          <td className="px-6 py-4 whitespace-nowrap last:pb-5">
+            Rp 520000
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+    )
+}
