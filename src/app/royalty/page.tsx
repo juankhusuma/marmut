4d9b213dca47a12a4c946fccdbe2f3657ec6817b
codaaa19 <tsabitiphone@gmail.com>
2024-04-28 01:04:59 +0700

    Applying table for album-song label + user(not finish yet)

--- src/app/royalty/page.tsx
+++ src/app/royalty/page.tsx
@@ -1,246 +1,246 @@
 export default function Royalti(){
     return (
 <div className="flex justify-center items-center h-screen bg-gray-625" style={{ paddingTop: 'var(--header-height, 5rem)' }}>
-  <div className="overflow-hidden shadow-md sm:rounded-lg w-3/4 lg:w-1/2">
+  <div className="overflow-hidden shadow-md sm:rounded-lg w-3/4 lg:w-1/2 mb-10">
     <table className="min-w-full divide-y divide-gray-200">
-      <thead className="bg-gray-50">
+      <thead className="bg-deep-blue">
         <tr>
-          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-white opacity-80 uppercase tracking-wider">
             Judul Lagu
           </th>
-          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-white opacity-80 uppercase tracking-wider">
             Judul Album
           </th>
-          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-white opacity-80 uppercase tracking-wider">
             Total Play
           </th>
-          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-white opacity-80 uppercase tracking-wider">
             Total Download
           </th>
-          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-white opacity-80 uppercase tracking-wider">
             Total Royalti Didapat
           </th>
         </tr>
       </thead>
       <tbody className="bg-white divide-y divide-gray-200">
         <tr>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap  text-deep-blue opacity-75">
             Lagu1
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap  text-deep-blue opacity-75">
             Album1
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap  text-deep-blue opacity-75">
             3
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap  text-deep-blue opacity-75">
             0
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap  text-deep-blue opacity-75">
             Rp 450000
           </td>
         </tr>
         <tr>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap  text-deep-blue opacity-75">
             Lagu2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Album2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Rp 520000
           </td>
         </tr>
         <tr>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Lagu2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Album2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Rp 520000
           </td>
         </tr>
         <tr>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Lagu2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Album2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Rp 520000
           </td>
         </tr>
         <tr>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Lagu2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Album2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Rp 520000
           </td>
         </tr>
         <tr>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Lagu2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Album2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Rp 520000
           </td>
         </tr>
         <tr>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Lagu2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Album2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Rp 520000
           </td>
         </tr>
         <tr>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Lagu2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Album2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Rp 520000
           </td>
         </tr>
         <tr>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Lagu2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Album2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Rp 520000
           </td>
         </tr>
         <tr>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Lagu2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Album2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Rp 520000
           </td>
         </tr>
         <tr>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Lagu2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Album2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Rp 520000
           </td>
         </tr>
         <tr>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Lagu2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Album2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Rp 520000
           </td>
         </tr>
         <tr>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Lagu2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Album2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             2
           </td>
-          <td className="px-6 py-4 whitespace-nowrap last:pb-5">
+          <td className="px-6 py-4 whitespace-nowrap text-deep-blue opacity-75">
             Rp 520000
           </td>
         </tr>
