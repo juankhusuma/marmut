e8e490ce9951c4dba54dd0069f995685551d0ead
codaaa19 <tsabitiphone@gmail.com>
2024-05-02 14:21:14 +0700

    [REFACTOR] Cleaning structure

--- src/app/create-album/page.tsx
+++ src/app/create-album/page.tsx
@@ -13,16 +13,14 @@ export default function CreateAlbum() {
                             id="title"
                             name="title"
                             placeholder="Enter your album title here"
-                            className="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                        />
+                            className="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"/>
                     </div>
                     <div className="mb-4">
                         <label htmlFor="label" className="block text-sm font-medium text-gray-700">Label:</label>
                         <select
                             id="label"
                             name="label"
-                            className="mt-1 block w-full pl-3 pr-10 py-2 text-base border border-gray-300 bg-gray-50 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                        >
+                            className="mt-1 block w-full pl-3 pr-10 py-2 text-base border border-gray-300 bg-gray-50 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500">
                             <option>Choose a label</option>
                             <option value="label1">Label 1</option>
                             <option value="label2">Label 2</option>
@@ -31,8 +29,7 @@ export default function CreateAlbum() {
                     </div>
                     <button
                         type="submit"
-                        className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    >
+                        className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                         SUBMIT
                     </button>
                 </form>
