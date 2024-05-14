ce7cfc89048213a837f1f4e2785fd5368a9aad6e
juankhusuma <66712975+juankhusuma@users.noreply.github.com>
2024-05-14 20:38:09 +0700

    feat: register label

--- src/app/auth/register/label/page.tsx
+++ src/app/auth/register/label/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { handleRegisterLabel } from "@/action/handleRegisterLabel";
+import { triggerToast } from "@/utils/toast";
+import { FormEvent } from "react";
+
 export default function LabelRegisterPage() {
     return (
         <div className="flex justify-center items-center p-10">
-            <form className="form-control w-1/3">
+            <form onSubmit={handleSubmit} className="form-control w-1/3">
                 <h1 className="text-white font-bold text-center mb-5">Form Registrasi</h1>
                 <label htmlFor="email" className="label-text">Email</label>
                 <input className="input-sm input-primary mb-2" type="text" name="email" />
@@ -16,4 +22,16 @@ export default function LabelRegisterPage() {
             </form>
         </div >
     )
+}
+
+async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    try {
+        await handleRegisterLabel(formData);
+    } catch {
+        triggerToast("error", "Registration failed!");
+        return;
+    }
+    triggerToast("success", "Registration success!");
 }
---
+++ src/app/songs/create/page.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export default function CreateSong() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-900">
+            <div className="bg-white p-6 rounded-lg shadow-lg text-gray-900 max-w-md w-full">
+                <h3 className="text-lg font-semibold text-center mb-4">CREATE LAGU</h3>
+                <form>
+                    <div className="mb-4">
+                        <label htmlFor="album" className="block text-sm font-medium text-gray-700">Album:</label>
+                        <input type="text" id="album" name="album" placeholder="Input the album..." className="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" disabled />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="judul" className="block text-sm font-medium text-gray-700">Judul:</label>
+                        <input type="text" id="judul" name="judul" placeholder="Input the song title..." className="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="artist" className="block text-sm font-medium text-gray-700">Artist:</label>
+                        <select id="artist" name="artist" className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 bg-gray-50 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500">
+                            <option value="" disabled selected>Choose an artist</option>
+                            <option value="artist1">Artist 1</option>
+                            <option value="artist2">Artist 2</option>
+                        </select>
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="genre" className="block text-sm font-medium text-gray-700">Songwriter :</label>
+                        <select id="genre" name="genre" className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 bg-gray-50 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500">
+                            <option value="" disabled selected>Choose songwriter</option>
+                            <option value="artist1">Songwriter 1</option>
+                            <option value="artist2">Songwriter 2</option>
+                        </select>
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="genre" className="block text-sm font-medium text-gray-700">Genre:</label>
+                        <select id="genre" name="genre" className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 bg-gray-50 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500">
+                            <option value="" disabled selected>Choose genre</option>
+                            <option value="artist1">Genre 1</option>
+                            <option value="artist2">Genre 2</option>
+                        </select>
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="durasi" className="block text-sm font-medium text-gray-700">Durasi:</label>
+                        <input type="text" id="durasi" name="durasi" placeholder="Input the song duration..." className="mt-1 block w-full px-3 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
+                    </div>
+                    <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        SUBMIT
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
